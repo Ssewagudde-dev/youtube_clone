1ad900bda433235cc17b1976ad9a65d4739d497e
Ssewagudde David <dsewagude10@gmail.com>
2024-05-01 22:49:16 +0300

    Page header

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PageHeader from "./layout/PageHeader";
+
+const App = () => {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+    </div>
+  );
+};
+
+export default App;
